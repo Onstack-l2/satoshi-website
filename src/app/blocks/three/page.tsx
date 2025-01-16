6f9db8a00de92f273face8e4d1f0f16d45aa3e93
tuando <tuan.do@mita.games>
2025-01-16 08:58:41 +0000

    Refactor block three page

--- src/app/blocks/three/page.tsx
+++ src/app/blocks/three/page.tsx
@@ -1,175 +1,72 @@
+import DesktopPageNavigation from "@/app/ui/DesktopPageNavigation";
 import IconImage from "@/app/ui/IconImage";
 import TriangelBox from "@/app/ui/TriangelBox";
-import clsx from "clsx";
+import BlockThreeBg from "@/app/ui/background/BlockThreeBg";
 
 import Image from "next/image";
 import Link from "next/link";
 
-const ImageLine = ({ className }: { className?: string }) => {
-  return (
-    <div className={clsx("w-[1444px]", className)}>
-      <Image src="/icons/line.svg" alt="" height={75} width={2888} />
-    </div>
-  );
-};
-
-const Background = ({ className }: { className?: string }) => {
-  return (
-    <div
-      className={clsx(
-        "w-[1444px] relative h-[712px] z-0 py-[20px] mt-[44px]",
-        className
-      )}
-    >
-      <IconImage
-        iconName="btc-icon-bg.svg"
-        width={90}
-        height={89}
-        className="absolute top-[83px] left-[22px] hidden xl-custom:block"
-      />
-
-      <IconImage
-        iconName="3coins-icon-bg.svg"
-        width={64}
-        height={67}
-        className="absolute top-[346px] left-[22px]"
-      />
-      <IconImage
-        iconName="money-icon-bg.svg"
-        width={95}
-        height={121}
-        className="absolute top-[78px] left-[45%] hidden sm:block"
-      />
-
-      <IconImage
-        iconName="3coins-icon-bg.svg"
-        width={81}
-        height={84}
-        className="absolute top-[65px] right-[145px] hidden lg:block"
-      />
-
-      {/* <IconImage
-        iconName="bottle-icon-bg.svg"
-        width={71}
-        height={93}
-        className="absolute top-[197px] right-[304px]"
-      /> */}
-
-      <IconImage
-        iconName="btc-icon-bg.svg"
-        width={90}
-        height={89}
-        className="absolute bottom-[86px] right-[351px]"
-      />
-
-      <div className="z-0 absolute bottom-0 h-fit w-full flex">
-        <div className="relative w-full h-fit mt-[-5px]">
-          {[1, 2, 3, 4, 5, 6].map((num, index) => {
-            return (
-              <ImageLine
-                key={num}
-                className={clsx(
-                  index !== [1, 2, 3, 4, 5, 6].length - 1 && "mb-[-38px]"
-                )}
-              />
-            );
-          })}
-        </div>
-        <div className="relative w-full h-fit ">
-          {[1, 2, 3, 4, 5, 6].map((num, index) => {
-            return (
-              <ImageLine
-                key={num}
-                className={clsx(
-                  index !== [1, 2, 3, 4, 5, 6].length - 1 && "mb-[-38px]"
-                )}
-              />
-            );
-          })}
-        </div>
-        <div className="relative w-full h-fit mt-[5px]">
-          {[1, 2, 3, 4, 5, 6].map((num, index) => {
-            return (
-              <ImageLine
-                key={num}
-                className={clsx(
-                  index !== [1, 2, 3, 4, 5, 6].length - 1 && "mb-[-38px]"
-                )}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+const blockThreeContent = {
+  header: "Block Three",
+  title: "The Roadmap",
+  features: {
+    title: "We’re building the future of DeFi, one tap at a time.",
+  },
+  statges: [
+    {
+      title: "Start the Revolution",
+      description:
+        "Launch core gameplay (mining, raiding, Cooperation Hubs.Hit 3M+ players.",
+      phase: "Phase 1",
+    },
+    {
+      title: "Unlock DeFi Power",
+      description:
+        "Integrate staking and yield farming, copy trading and more to Drive $50M+ in TVL.",
+      phase: "Phase 2",
+    },
+    {
+      title: "BTCFi Domination",
+      description:
+        "Full BTCFi to TON to SOL to SUI blockchain integration. \nAchieve 10M players and $500M TVL.",
+      phase: "Phase 3",
+    },
+  ],
 };
 
 export default function BlockThree() {
   return (
     <>
       <div className="max-w-7xl w-full p-6 mx-auto z-20 flex flex-col lg:flex-row flex-1">
-        <div
-          className="static hidden lg:flex lg:absolute top-[50%] z-40 w-full left-0"
-          style={{
-            transform: "translateY(-50%)",
-          }}
-        >
-          <Link href="/blocks/two" className="relative flex items-center">
-            <IconImage
-              iconName="play-icon.svg"
-              width={119}
-              height={119}
-              alt=""
-              className="mt-5"
-              flipHorizontal={true}
-            />
-            <IconImage
-              iconName="arrow-icon-bg.svg"
-              width={70}
-              height={80}
-              className="mb-10"
-              flipHorizontal={true}
-            />
-          </Link>
-          <Link
-            href="/blocks/defi"
-            className="relative flex items-center ml-auto"
-          >
-            <IconImage
-              iconName="arrow-icon-bg.svg"
-              width={70}
-              height={80}
-              className="mb-10"
-            />
-            <IconImage
-              iconName="play-icon.svg"
-              width={119}
-              height={119}
-              alt=""
-              className="mt-5"
-            />
-          </Link>
-        </div>
+        <DesktopPageNavigation prevPath="/blocks/two" nextPath="/blocks/defi" />
 
         <div className="w-full flex flex-1">
           <div className="flex flex-col w-full gap-[32px] z-20 text-center items-center lg:items-start ">
-            <h2 className="text-[2rem] text-white mt-8">Block Three</h2>
+            <h2 className="text-[2rem] text-white mt-8">
+              {blockThreeContent.header}
+            </h2>
             <h1 className="text-[#E8B14B] text-5xl max-w-[518px]">
-              The Roadmap
+              {blockThreeContent.title}
             </h1>
 
             <div className="flex gap-[26px]">
               <div className="flex flex-col gap-[34px] items-start text-end w-[70px]">
                 <div className="flex flex-col gap-1 w-[70px]">
-                  <span className="text-white text-base">Phase 1</span>
+                  <span className="text-white text-base">
+                    {blockThreeContent.statges[0].phase}
+                  </span>
                   <span className="text-transparent text-xs min-h-[32px]"></span>
                 </div>
                 <div className="flex flex-col gap-1 w-[70px]">
-                  <span className="text-white text-base">Phase 2</span>
+                  <span className="text-white text-base">
+                    {blockThreeContent.statges[1].phase}
+                  </span>
                   <span className="text-transparent text-xs min-h-[48px]"></span>
                 </div>
                 <div className="flex flex-col gap-1 w-[70px]">
-                  <span className="text-white text-base">Phase 3</span>
+                  <span className="text-white text-base">
+                    {blockThreeContent.statges[2].phase}
+                  </span>
                   <span className="text-transparent text-xs min-h-[48px]"></span>
                 </div>
               </div>
@@ -183,33 +80,16 @@ export default function BlockThree() {
               </div>
 
               <div className="flex flex-col gap-[34px] items-start text-start sm:w-[255px]">
-                <div className="flex flex-col gap-1 sm:w-[255px]">
-                  <span className="text-[#E8B14B] text-base">
-                    Start the Revolution
-                  </span>
-                  <span className="text-[#E38539] text-xs">
-                    Launch core gameplay (mining, raiding, Cooperation Hubs.Hit
-                    3M+ players.
-                  </span>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <span className="text-[#E8B14B] text-base">
-                    Unlock DeFi Power
-                  </span>
-                  <span className="text-[#E38539] text-xs">
-                    Integrate staking and yield farming, copy trading and more
-                    to Drive $50M+ in TVL.
-                  </span>
-                </div>
-                <div className="flex flex-col gap-1">
-                  <span className="text-[#E8B14B] text-base">
-                    BTCFi Domination
-                  </span>
-                  <span className="text-[#E38539] text-xs">
-                    Full BTCFi to TON to SOL to SUI blockchain integration.
-                    Achieve 10M players and $500M TVL.
-                  </span>
-                </div>
+                {blockThreeContent.statges.map((item, index) => (
+                  <div className="flex flex-col gap-1 sm:w-[255px]" key={index}>
+                    <span className="text-[#E8B14B] text-base">
+                      {item.title}
+                    </span>
+                    <span className="text-[#E38539] text-xs whitespace-pre-line">
+                      {item.description}
+                    </span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -260,7 +140,7 @@ export default function BlockThree() {
           <div className="h-fit w-[505px] max-w-full text-left">
             <TriangelBox>
               <h5 className="text-[#E8B14B] text-base">
-                We’re building the future of DeFi, one tap at a time.
+                {blockThreeContent.features.title}
               </h5>
             </TriangelBox>
 
@@ -278,7 +158,7 @@ export default function BlockThree() {
       </div>
 
       <div className="absolute bottom-0 right-0 w-full overflow-hidden flex justify-end">
-        <Background className="!h-[calc(100vh - 170px)] !w-full" />
+        <BlockThreeBg className="!h-[calc(100vh - 170px)] !w-full" />
       </div>
     </>
   );
