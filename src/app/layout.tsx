271fcf76a76db410e65855b625fa4dadf59945c8
tuando <tuan.do@mita.games>
2025-01-17 03:15:02 +0000

    Add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,36 @@ export const anonymousPro = Anonymous_Pro({
 export const metadata: Metadata = {
   title:
     "Satoshi Miner: Farm Bits Tokens | earn Yields and Stake your way to crypto glory all from your phone",
-  description: "Satoshi Miner",
+  description:
+    "Satoshi Miner transforms casual gaming into DeFi, blending the thrill of farming with the power of gaming!",
+  openGraph: {
+    title:
+      "Satoshi Miner: Farm Bits Tokens | earn Yields and Stake your way to crypto glory all from your phone",
+    description:
+      "Satoshi Miner transforms casual gaming into DeFi, blending the thrill of farming with the power of gaming!",
+    url: "https://satoshiminer.io",
+    siteName: "Satoshi Miner: Farm Bits Tokens",
+    images: [
+      {
+        url: "https://satoshi-website.vercel.app/images/logo.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "Satoshi Miner: Farm Bits Tokens | earn Yields and Stake your way to crypto glory all from your phone",
+    description:
+      "Satoshi Miner transforms casual gaming into DeFi, blending the thrill of farming with the power of gaming!",
+    siteId: "1545263305027092480",
+    // creator: "@nextjs",
+    // creatorId: "1467726470533754880",
+    images: ["https://satoshi-website.vercel.app/images/logo.png"], // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({
